e37af233c5dbbd351f1f27c0a164c65ddf8d0a9e
Nick Beaudoin <believewithyoureyes@gmail.com>
2019-11-16 18:46:40 -0500

    preliminary clean up

---
+++ src/App/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+class App extends React.Component {
+  
+  render() {
+    return (
+      <div >
+        
+      </div>
+    );
+  }
+
+}
+
+export default App;
+
+
+/*
+ * this app has a few goals:
+ * - rebuild my existing memory game using react
+ * - use component state
+ * 
+ * 
+ * 
+ * Next app will be same memory game, but with redux
+ */
