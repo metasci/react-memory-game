64da626e3b52a9c976c12b6752b728ed55cc29e0
Nick Beaudoin <believewithyoureyes@gmail.com>
2019-11-18 21:49:39 -0500

    card can now be flipped via boolean prop

--- src/App/Board/Card/Card.js
+++ src/App/Board/Card/Card.js
@@ -4,7 +4,7 @@ import styles from './Card.module.css';
 class Card extends React.Component {
     render() {
         return (
-            <div className={styles.card}>
+            <div className={styles.card + ' ' + (this.props.flipped ? styles.flipped : '')} onClick={this.props.onClick}>
                 <div className={styles.front}></div>
                 <div className={styles.back}></div>
             </div>
